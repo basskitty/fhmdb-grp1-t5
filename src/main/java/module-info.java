6a03377459a726091acfa8b9129c2bf6b0e838a0
basskitty <sleepyskeleton@gmail.com>
2025-03-25 15:56:43 +0100

    added gson + okhttp to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,11 @@ module at.ac.fhcampuswien.fhmdb {
     requires javafx.fxml;
     requires org.controlsfx.controls;
     requires com.jfoenix;
+    requires okhttp3;
+    requires com.google.gson;
 
-    opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
+    opens at.ac.fhcampuswien.fhmdb;
+    opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;
 
     exports at.ac.fhcampuswien.fhmdb;
     exports at.ac.fhcampuswien.fhmdb.models;
